e62da5ac06023d5003514bbc8e0e1cdb0e740bdf
liamjosephnolan <liamjosephnolan@gmail.com>
2025-04-03 11:33:04 +0200

    better sensor data format

--- src/main.cpp
+++ src/main.cpp
@@ -5,170 +5,233 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-#include <std_msgs/msg/string.h> // Use String message type
+#include <sensor_msgs/msg/joint_state.h>
+#include <std_msgs/msg/float32_multi_array.h>
 
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 #error This example is only available for Arduino framework with serial transport.
 #endif
 
-// Pin definitions for the encoders
-#define JOINT1_CS_PIN 33  // Joint 1 Chip Select
-#define JOINT1_CLK_PIN 34 // Joint 1 Clock
-#define JOINT1_DO_PIN 35  // Joint 1 Data
+// Hardware Pin Definitions
+#define JOINT1_CS_PIN 33
+#define JOINT1_CLK_PIN 34 
+#define JOINT1_DO_PIN 35
 
-#define JOINT2_CS_PIN 36  // Joint 2 Chip Select
-#define JOINT2_CLK_PIN 37 // Joint 2 Clock
-#define JOINT2_DO_PIN 38  // Joint 2 Data
+#define JOINT2_CS_PIN 36
+#define JOINT2_CLK_PIN 37
+#define JOINT2_DO_PIN 38
 
-#define JOINT3_CS_PIN 39 // Joint 3 Chip Select
-#define JOINT3_CLK_PIN 40 // Joint 3 Clock
-#define JOINT3_DO_PIN 41  // Joint 3 Data
+#define JOINT3_CS_PIN 39
+#define JOINT3_CLK_PIN 40
+#define JOINT3_DO_PIN 41
 
-// Pin definitions for the gimbal
-#define POT_1_PIN 15 
-#define POT_2_PIN 16
-#define POT_3_PIN 17
-#define HALL_PIN 18
+#define POT_1_PIN 15  // right_gimbal_1
+#define POT_2_PIN 16  // right_gimbal_2
+#define POT_3_PIN 19  // right_gimbal_3
+#define HALL_PIN 18   // right_gimbal_0
 
+// Analog voltage supply pins (Must be set to high to supply gimbal with power
+#define SUPPLY_PIN_1 22 //3.3V voltage supply pin
+#define SUPPLY_PIN_2 23 //3.3V voltage supply pin
 
-
-
-
-// Publishers for joint states and analog values
+// ROS 2 Components
 rcl_publisher_t joint_state_publisher;
-rcl_publisher_t analog_value_publisher;
-
-// Messages for joint states and analog values
-std_msgs__msg__String joint_state_msg;
-std_msgs__msg__String analog_value_msg;
-
+rcl_publisher_t raw_values_publisher;
+sensor_msgs__msg__JointState joint_state_msg;
+std_msgs__msg__Float32MultiArray raw_values_msg;
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 
+// =====================================================================
+// JOINT CALIBRATION DATA
+// =====================================================================
+struct JointCalibration {
+  float min_raw;    // Minimum raw sensor reading
+  float max_raw;    // Maximum raw sensor reading
+  float min_angle;  // Minimum angle in radians
+  float max_angle;  // Maximum angle in radians
+  float exp_a, exp_b, exp_c, exp_d; // For hall effect
+};
+
+JointCalibration calib[7] = {
+  // Encoder Joints (12-bit absolute encoders)
+  {0, 4095, 0, 2*M_PI},  // right_joint_1 (0-360°)
+  {0, 4095, 0, 2*M_PI},  // right_joint_2
+  {0, 4095, 0, 2*M_PI},  // right_joint_3
+  
+  // Potentiometer Joints (reverse order)
+  {94, 860, -125.0*M_PI/180.0, 125.0*M_PI/180.0},  // right_gimbal_3 (Pot3)
+  {72, 930, -40.0*M_PI/180.0, 40.0*M_PI/180.0},    // right_gimbal_2 (Pot2)
+  {20, 910, -40.0*M_PI/180.0, 40.0*M_PI/180.0},    // right_gimbal_1 (Pot1)
+  
+  // Hall Effect (right_gimbal_0)
+  {555, 973, 0, 20.0*M_PI/180.0, 31.46, 0.000225, -4.767e5, -0.0189}
+};
+// =====================================================================
+
+// Error handling macros
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-// Error handle loop
 void error_loop() {
-  while (1) {
-    delay(100); // Keep this delay for error handling (non-critical path)
-  }
+  while(1) { delay(100); }
 }
 
-// Encoder reading function
 void readEncoder(unsigned int *OutData, unsigned int DO, int CSn, unsigned int CLK) {
-  *OutData = 0;  // Reset Output Array.
+  *OutData = 0;
   digitalWrite(CSn, LOW);
-  delayMicroseconds(1);  // Waiting for Tclkfe=500ns Or 16 times NOP
-  // Passing 12 times, from 0 to 11
-  for (int x = 0; x < 12; x++) {
+  delayMicroseconds(1);
+  for(int x=0; x<12; x++) {
     digitalWrite(CLK, LOW);
-    delayMicroseconds(1);  // Tclk/2_min = 500ns = 8 * 62.5
+    delayMicroseconds(1);
     digitalWrite(CLK, HIGH);
-    delayMicroseconds(1);  // Tdo valid, like Tclk/2
+    delayMicroseconds(1);
     *OutData = (*OutData << 1) | digitalRead(DO);
   }
-  digitalWrite(CSn, HIGH);  // Deselects the encoder from reading
+  digitalWrite(CSn, HIGH);
+}
+
+float map_exponential(float x, const JointCalibration &c) {
+  return c.exp_a*exp(c.exp_b*x) + c.exp_c*exp(c.exp_d*x);
 }
 
 void setup() {
-  // Configure serial transport
   Serial.begin(115200);
   set_microros_serial_transports(Serial);
-  delay(2000); // Initial delay for micro-ROS setup (only once)
+  delay(2000);
 
-  // Configure encoder pins
+  // Configure pins
   pinMode(JOINT1_CS_PIN, OUTPUT);
   pinMode(JOINT1_CLK_PIN, OUTPUT);
   pinMode(JOINT1_DO_PIN, INPUT);
-
   pinMode(JOINT2_CS_PIN, OUTPUT);
   pinMode(JOINT2_CLK_PIN, OUTPUT);
   pinMode(JOINT2_DO_PIN, INPUT);
-
   pinMode(JOINT3_CS_PIN, OUTPUT);
   pinMode(JOINT3_CLK_PIN, OUTPUT);
   pinMode(JOINT3_DO_PIN, INPUT);
+  pinMode(SUPPLY_PIN_1, OUTPUT);      // Set pin 22 as an output
+  pinMode(SUPPLY_PIN_2, OUTPUT);      // Set pin 22 as an output
+  digitalWrite(JOINT1_CS_PIN, HIGH);
+  digitalWrite(JOINT2_CS_PIN, HIGH);
+  digitalWrite(JOINT3_CS_PIN, HIGH);
+  digitalWrite(SUPPLY_PIN_1,HIGH);
+  digitalWrite(SUPPLY_PIN_2,HIGH);
 
-  digitalWrite(JOINT1_CS_PIN, HIGH); // Disable the encoder initially
-  digitalWrite(JOINT2_CS_PIN, HIGH); // Disable the encoder initially
-  digitalWrite(JOINT3_CS_PIN, HIGH); // Disable the encoder initially
-
-
-  pinMode(22, OUTPUT);      // Set pin 22 as an output
-  digitalWrite(22, HIGH);  // Set pin 22 to HIGH (3.3V
-  pinMode(23, OUTPUT);      // Set pin 23 as an output
-  digitalWrite(23, HIGH);  // Set pin 23 to HIGH (3.3V
-
+  // Initialize ROS 2
   allocator = rcl_get_default_allocator();
-
-  // Create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "mtm_arm_node", "", &support));
 
-  // Create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
-
-  // Create publishers
+  // Initialize joint state publisher
   RCCHECK(rclc_publisher_init_default(
     &joint_state_publisher,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), // Use String message type
-    "mtm_joint_states")); // Publish to the mtm_joint_states topic
+    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),
+    "mtm_joint_states"));
 
+  // Initialize raw values publisher
   RCCHECK(rclc_publisher_init_default(
-    &analog_value_publisher,
+    &raw_values_publisher,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), // Use String message type
-    "mtm_analog_values")); // Publish to the mtm_analog_values topic
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+    "mtm_raw_values"));
 
-  // Initialize the messages
-  joint_state_msg.data.data = (char*)malloc(100); // Allocate memory for the string message
-  joint_state_msg.data.capacity = 100; // Set the maximum capacity of the string
+  // Setup joint state message
+  joint_state_msg.name.size = 7;
+  joint_state_msg.name.data = (rosidl_runtime_c__String*)malloc(7 * sizeof(rosidl_runtime_c__String));
+  
+  const char* joint_names[] = {
+    "right_joint_1", "right_joint_2", "right_joint_3",
+    "right_gimbal_3", "right_gimbal_2", "right_gimbal_1", 
+    "right_gimbal_0"
+  };
+  
+  for(int i=0; i<7; i++) {
+    joint_state_msg.name.data[i].data = (char*)malloc(20);
+    strcpy(joint_state_msg.name.data[i].data, joint_names[i]);
+    joint_state_msg.name.data[i].size = strlen(joint_names[i]);
+    joint_state_msg.name.data[i].capacity = 20;
+  }
+  
+  joint_state_msg.position.size = 7;
+  joint_state_msg.position.data = (double*)malloc(7 * sizeof(double));
 
-  analog_value_msg.data.data = (char*)malloc(100); // Allocate memory for the string message
-  analog_value_msg.data.capacity = 100; // Set the maximum capacity of the string
+  // Setup raw values message
+  raw_values_msg.data.size = 7;
+  raw_values_msg.data.data = (float*)malloc(7 * sizeof(float));
+  raw_values_msg.layout.dim.size = 1;
+  raw_values_msg.layout.dim.data = (std_msgs__msg__MultiArrayDimension*)malloc(sizeof(std_msgs__msg__MultiArrayDimension));
+  raw_values_msg.layout.dim.data[0].label.data = (char*)"raw_values";
+  raw_values_msg.layout.dim.data[0].label.size = strlen("raw_values");
+  raw_values_msg.layout.dim.data[0].size = 7;
+  raw_values_msg.layout.dim.data[0].stride = 7;
 
-  // Initialize the executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
 }
 
 void loop() {
+  // Read all sensor values
+  unsigned int encoderValues[3];
+  readEncoder(&encoderValues[0], JOINT1_DO_PIN, JOINT1_CS_PIN, JOINT1_CLK_PIN);
+  readEncoder(&encoderValues[1], JOINT2_DO_PIN, JOINT2_CS_PIN, JOINT2_CLK_PIN);
+  readEncoder(&encoderValues[2], JOINT3_DO_PIN, JOINT3_CS_PIN, JOINT3_CLK_PIN);
   
-  Serial.println("AHHHHHHHH");
-  // Read the gimbal values
-  int Pot1Value, Pot2Value, Pot3Value, HallValue;
-  Pot1Value = analogRead(POT_1_PIN); // Read analog value from POT_1_PIN
-  Pot2Value = analogRead(POT_2_PIN); // Read analog value from POT_2_PIN
-  Pot3Value = analogRead(POT_3_PIN); // Read analog value from POT_3_PIN
-  HallValue = analogRead(HALL_PIN);  // Read analog value from HALL_PIN
+  int potValues[3] = {
+    analogRead(POT_1_PIN),  // right_gimbal_1
+    analogRead(POT_2_PIN),  // right_gimbal_2
+    analogRead(POT_3_PIN)   // right_gimbal_3
+  };
+  int hallValue = analogRead(HALL_PIN);  // right_gimbal_0
 
-  // Format the analog values message
-  snprintf(analog_value_msg.data.data, analog_value_msg.data.capacity, "Pot1: %d, Pot2: %d, Pot3: %d, Hall: %d", Pot1Value, Pot2Value, Pot3Value, HallValue);
-  analog_value_msg.data.size = strlen(analog_value_msg.data.data); // Update the size of the string
+  // Publish raw values (order matches joint_state names)
+  raw_values_msg.data.data[0] = encoderValues[0];
+  raw_values_msg.data.data[1] = encoderValues[1];
+  raw_values_msg.data.data[2] = encoderValues[2];
+  raw_values_msg.data.data[3] = potValues[2];  // gimbal_3
+  raw_values_msg.data.data[4] = potValues[1];  // gimbal_2
+  raw_values_msg.data.data[5] = potValues[0];  // gimbal_1
+  raw_values_msg.data.data[6] = hallValue;     // gimbal_0
+  RCSOFTCHECK(rcl_publish(&raw_values_publisher, &raw_values_msg, NULL));
 
-  // Publish the analog values message
-  RCSOFTCHECK(rcl_publish(&analog_value_publisher, &analog_value_msg, NULL));
+  // Convert to angles (radians first)
+  float positions_rad[7];
+  
+  // Encoder joints
+  for(int i=0; i<3; i++) {
+    positions_rad[i] = (encoderValues[i] - calib[i].min_raw) / 
+                      (calib[i].max_raw - calib[i].min_raw) *
+                      (calib[i].max_angle - calib[i].min_angle) + 
+                      calib[i].min_angle;
+  }
+  
+  // Potentiometer joints (reverse order)
+  for(int i=0; i<3; i++) {
+    positions_rad[6-i] = (potValues[i] - calib[3+i].min_raw) / 
+                        (calib[3+i].max_raw - calib[3+i].min_raw) *
+                        (calib[3+i].max_angle - calib[3+i].min_angle) + 
+                        calib[3+i].min_angle;
+  }
+  
+  // Hall effect
+  positions_rad[6] = map_exponential(hallValue, calib[6]) * 
+                    (calib[6].max_angle - calib[6].min_angle) + 
+                    calib[6].min_angle;
 
-  // Read the encoder values
-  unsigned int encoderValue1, encoderValue2, encoderValue3;
-  readEncoder(&encoderValue1, JOINT1_DO_PIN, JOINT1_CS_PIN, JOINT1_CLK_PIN);
-  readEncoder(&encoderValue2, JOINT2_DO_PIN, JOINT2_CS_PIN, JOINT2_CLK_PIN);
-  readEncoder(&encoderValue3, JOINT3_DO_PIN, JOINT3_CS_PIN, JOINT3_CLK_PIN);
+  // Convert to degrees for publishing
+  for(int i=0; i<7; i++) {
+    joint_state_msg.position.data[i] = positions_rad[i] * 180.0 / M_PI;
+  }
 
-  // Convert the encoder values to floating-point angles (assuming 12-bit encoder)
-  float angle1 = (encoderValue1 * 360.0f) / 4096.0f; // Joint 1 angle
-  float angle2 = (encoderValue2 * 360.0f) / 4096.0f; // Joint 2 angle
-  float angle3 = (encoderValue3 * 360.0f) / 4096.0f; // Joint 3 angle
+  // Update timestamp
+  joint_state_msg.header.stamp.sec = millis() / 1000;
+  joint_state_msg.header.stamp.nanosec = (millis() % 1000) * 1000000;
 
-  // Format the joint states message
-  snprintf(joint_state_msg.data.data, joint_state_msg.data.capacity, "joint1_angle: %.2f, joint2_angle: %.2f, joint3_angle: %.2f", angle1, angle2, angle3);
-  joint_state_msg.data.size = strlen(joint_state_msg.data.data); // Update the size of the string
-
-  // Publish the joint states message
+  // Publish joint states
   RCSOFTCHECK(rcl_publish(&joint_state_publisher, &joint_state_msg, NULL));
-
-  // Spin the executor to process callbacks
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(0))); // Spin with a small delay
+  
+  // Process callbacks
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 }
